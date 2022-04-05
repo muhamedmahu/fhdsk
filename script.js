25ceaa4f551ab4885cbc258ae2afc43358f2a3cd
muhamedmahu <91724573+muhamedmahu@users.noreply.github.com>
2022-04-05 16:43:18 +0000

    commit

---
+++ script.js
@@ -0,0 +1,4 @@
+const imageAfter = document.querySelector('cpmpare__after');
+const widthImageAfter = imageAfter.offsetWidth;
+const heightImageAfter = imageAfter.offsetWidth;
+const delimetr = document.querySelector('.compare__delimetr'); 
